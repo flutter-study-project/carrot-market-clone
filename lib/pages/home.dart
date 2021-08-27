9397831e0eeef76d60518bacd9a28964273402c5
minsoo kim <zlemzlem5656@naver.com>
2021-08-28 02:05:18 +0900

    [추가] bottomNavigationBar 추가

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,10 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  int _currentPageIndex = 0;
 
   @override
   void initState() {
     super.initState();
+    _currentPageIndex = 0;
     datas = [
       {
         "cid": "1",
@@ -198,11 +200,44 @@ class _HomeState extends State<Home> {
         itemCount: datas.length);
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(String iconName, String label) {
+    return BottomNavigationBarItem(
+        icon: Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: SvgPicture.asset(
+            'assets/svg/$iconName.svg',
+            width: 22,
+          ),
+        ),
+        label: label);
+  }
+
+  Widget _bottomNavigationBarWidget() {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        currentIndex: _currentPageIndex,
+        selectedItemColor: Colors.black,
+        selectedFontSize: 12,
+        items: [
+          _bottomNavigationBarItem('home_off', '홈'),
+          _bottomNavigationBarItem('notes_off', '동내생활'),
+          _bottomNavigationBarItem('location_off', '내 근처'),
+          _bottomNavigationBarItem('chat_off', '채팅'),
+          _bottomNavigationBarItem('user_off', '나의 당근'),
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbarwidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomNavigationBarWidget(),
       // bottomNavigationBar: Container(),
     );
   }
