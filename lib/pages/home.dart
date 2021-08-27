1e8f8e3614b8e1d24a35b9edf8f68140808716c1
minsoo kim <zlemzlem5656@naver.com>
2021-08-28 01:39:08 +0900

    [추가] 리스트 뷰 퍼블리싱

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -9,6 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Map<String, String>> datas = [];
+
   @override
   void initState() {
     super.initState();
@@ -23,7 +26,7 @@ class _HomeState extends State<Home> {
       },
       {
         "cid": "2",
-        "image": "assets/im ages/ara-2.jpg",
+        "image": "assets/images/ara-2.jpg",
         "title": "LA갈비 5kg팔아요~",
         "location": "제주 제주시 아라동",
         "price": "100000",
@@ -122,11 +125,77 @@ class _HomeState extends State<Home> {
     );
   }
 
+  String calcStringToWon(String priceString) {
+    final oCcy = new NumberFormat("#,###", "ko_KR");
+    return '${oCcy.format(int.parse(priceString))}원';
+  }
+
   Widget _bodyWidget() {
     return ListView.separated(
-        itemBuilder: itemBuilder,
-        separatorBuilder: separatorBuilder,
-        itemCount: itemCount)();
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      child: Image.asset(
+                        datas[index]["image"]!,
+                        width: 100,
+                        height: 100,
+                      )),
+                  Expanded(
+                    child: Container(
+                        height: 100,
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              datas[index]['title']!,
+                              style: TextStyle(fontSize: 15),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              datas[index]['location']!,
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.black.withOpacity(0.3)),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              calcStringToWon(datas[index]['price']!),
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                            Expanded(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    SvgPicture.asset(
+                                      'assets/svg/heart_off.svg',
+                                      width: 13,
+                                      height: 13,
+                                    ),
+                                    SizedBox(width: 5),
+                                    Text(datas[index]['likes']!)
+                                  ]),
+                            ),
+                          ],
+                        )),
+                  )
+                ],
+              ));
+        },
+        separatorBuilder: (context, index) {
+          return Container(height: 1, color: Colors.black);
+        },
+        itemCount: datas.length);
   }
 
   @override
@@ -134,7 +203,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: _appbarwidget(),
       body: _bodyWidget(),
-      bottomNavigationBar: Container(),
+      // bottomNavigationBar: Container(),
     );
   }
 }
